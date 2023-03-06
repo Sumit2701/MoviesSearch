87b72a1eff00773a526c201defa34901bbc51c25
Sumit2701 <76913300+Sumit2701@users.noreply.github.com>
2023-03-06 18:47:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from "react-dom";
+import { useEffect, useState } from "react";
+import "./style.css";
+import Moviesdata from './moviesdata'
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=dba7764d";
 
-function App() {
+
+const App = () => {
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const searchData = await response.json();
+    setmovies(searchData.Search);
+    console.log(movies)
+  };
+ 
+  
+  const[movies,setmovies]=useState([]);
+  const[searchTerm,setSearchTerm]=useState([]);
+  useEffect(() => {
+    searchMovie("Batman");
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Sumit's Movie world</h1>
+      <div className="search">
+        <input
+          value={searchTerm}
+          placeholder="Search your favourite movie"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+         <img
+          src={'./logo192.png'}
+          alt="search"
+          onClick={() => searchMovie(searchTerm)}
+        />
+
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <Moviesdata movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
-
+};
 export default App;
+
